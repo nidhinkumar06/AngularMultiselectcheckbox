cb65ea8ceaefcedac772abae22c6a606c8335f02
nidhinkumar <nidhinkumar@codingtown.com>
2020-01-25 22:27:32 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MultiSelectComponent } from './shared/components/multi-select/multi-select.component';
+import { ListFilterPipe } from './shared/components/multi-select/list-filter.pipe';
+import { ClickOutsideDirective } from './shared/directives/click-outside.directive';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiSelectComponent,
+    ListFilterPipe,
+    ClickOutsideDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
